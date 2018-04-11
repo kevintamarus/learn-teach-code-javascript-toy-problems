807cdf55a44d005dce26c1deb31a9609bc138d93
Kevin Tamarus Tang <kevintamarus@gmail.com>
2018-04-10 23:56:28 -0700

    array duplicates problem added

---
+++ array-duplicates.js
@@ -0,0 +1,17 @@
+// Given an array of integers, return true if the array contains duplicates, otherwise, return false.
+// [1,2,3,4,5] => false
+// [1,2,3,4,4] => true
+// [] => false
+// [4] => false
+// [5,2,12,4,6,2,120] => true
+// If input is not an array, return "Invalid Entry!"
+
+const areThereDuplicates = arr => {
+  // your code here
+}
+
+// Can you solve it in O(n)(linear) time constraint?
+
+const areThereDuplicates = arr => {
+  // your code here
+}
