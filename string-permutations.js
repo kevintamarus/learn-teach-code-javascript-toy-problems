e56c49c657f5e9449a297ad3c4623236febd3a3c
Kevin Tamarus Tang <kevintamarus@gmail.com>
2018-04-13 23:30:08 -0700

    string permutations question added

---
+++ string-permutations.js
@@ -0,0 +1,9 @@
+// Given a string, find all possible permutations of the string, return your answers in an array
+
+// 'abc' => ['abc', 'acb', 'bac', 'bca', 'cab', 'cba']
+// 'ab' => ['ab', 'ba']
+// '111' => ['111', '111', '111', '111', '111', '111']
+
+const allStringPermutations = str => {
+  // your code here
+}
