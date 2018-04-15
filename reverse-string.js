bc1e00dad40b08190570cfae1e619e430451c67a
Kevin Tamarus Tang <kevintamarus@gmail.com>
2018-04-15 13:11:58 -0700

    reverse string problem added

---
+++ reverse-string.js
@@ -0,0 +1,10 @@
+// can you write a function that reverses a string?
+// 'sample' => 'elpmas'
+// 'racecar' => 'racecar'
+// 'string' => 'gnirts'
+
+const reverseString = str => {
+  //your code here
+}
+
+//what is the time constraint( O(n) ) for your function?
