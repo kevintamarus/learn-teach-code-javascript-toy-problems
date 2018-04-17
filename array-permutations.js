6ca414cb048d75eb0d338358456df1beea26a173
Kevin Tamarus Tang <kevintamarus@gmail.com>
2018-04-17 02:16:35 -0700

    array permutations problem added

---
+++ array-permutations.js
@@ -0,0 +1,9 @@
+// Given an array of letters, find all possible permutations, return your answers in an array
+
+// [a, b, c] => [[a, b ,c], [a, c, b], [b, a, c], [b, c, a], [c, a, b], [c, b, a]]
+// [a, b] => [[a, b], [b, a]]
+// [a, a, a] => [[a, a, a], [a, a, a], [a, a, a], [a, a, a], [a, a, a], [a, a, a]]
+
+const allStringPermutations = str => {
+  // your code here
+}
