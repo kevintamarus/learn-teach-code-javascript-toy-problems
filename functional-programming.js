960f1de1d4055458a7f4e087cf3cb7f94835ac63
Kevin Tamarus Tang <kevintamarus@gmail.com>
2018-04-23 23:11:03 -0700

    easy functional programming problem added

---
+++ functional-programming.js
@@ -0,0 +1,11 @@
+// solve this challenge using multiple functions listed below
+
+// given an array of numbers, return an array with all the numbers multiplied by 5
+
+const arrayTimesFive = arr => {
+  // your code here
+}
+
+const numberTimesFive = n => {
+  // your code here
+}
