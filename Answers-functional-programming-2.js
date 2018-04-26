abd0bcb66d9e31bf6e6a75612c730c70d32b9481
Kevin Tamarus Tang <kevintamarus@gmail.com>
2018-04-25 18:22:54 -0700

    changed currency conversion to extra credit/advanced

--- Answers-functional-programming-2.js
+++ Answers-functional-programming-2.js
@@ -12,7 +12,7 @@ an additional 9% sales tax is added to the subtotal
 given the drink order below(drink array), calculate the total price of the customer's drinks
 
 note: price is listed in cents (ex: 1 dollar => price: 100),
-your function should return in currency format (ex: if total is 1000, you should return "$10.00")
+extra credit: your function should return in currency format (ex: if total is 1000, you should return "$10.00")
 
 there are many different ways to solve this, functional programming is at your disposal!
 */
