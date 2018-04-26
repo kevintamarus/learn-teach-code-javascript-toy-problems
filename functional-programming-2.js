1a7096f8b1ba8758391d59b3d2d0e1731c9d7911
Kevin Tamarus Tang <kevintamarus@gmail.com>
2018-04-25 17:09:20 -0700

    starbucks functional programming problem added

---
+++ functional-programming-2.js
@@ -0,0 +1,40 @@
+// solve this challenge using multiple functions
+
+/*
+you're at a starbucks, you are trying to calculate a customer's total drink price:
+
+small = regular price
+medium = 120% regular price
+large = 140% regular price
+
+an additional 9% sales tax is added to the subtotal
+
+given the drink order below(drink array), calculate the total price of the customer's drinks
+
+note: price is listed in cents (ex: 1 dollar => price: 100),
+your function should return in currency format (ex: if total is 1000, you should return "$10.00")
+
+there are many different ways to solve this, functional programming is at your disposal!
+*/
+
+const drinks = [
+  {
+    name: "coffee",
+    price: 300,
+    size: "small"
+  },
+  {
+    name: "tea",
+    price: 200,
+    size: "medium"
+  },
+  {
+    name: "hot chocolate",
+    price: 250,
+    size: "large"
+  }
+]
+
+const totalPrice = arr => {
+  //your code here
+}
