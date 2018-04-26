dc45c6f557e2457b08df2ef19593d98f09f07038
Kevin Tamarus Tang <kevintamarus@gmail.com>
2018-04-26 03:38:54 -0700

    return fullnames added

---
+++ return-fullnames.js
@@ -0,0 +1,26 @@
+/*
+given an array called "names", which is an array of objects, return each object that has a last name
+
+your output should return => ["John Smith", "Jane Doe"]
+
+extra credit: use .reduce to solve this challenge!
+*/
+
+const names = [
+  {
+    first: "John",
+    last: "Smith"
+  },
+  {
+    first: "James",
+    last: ""
+  },
+  {
+    first: "Jane",
+    last: "Doe"
+  },
+]
+
+const printFullNames = names => {
+  // your code here
+}
