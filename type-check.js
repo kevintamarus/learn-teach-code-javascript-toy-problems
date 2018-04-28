6b42011b52bcfa794cb1976735fb398aef44965c
Kevin Tamarus Tang <kevintamarus@gmail.com>
2018-04-27 19:04:04 -0700

    type check problem added

---
+++ type-check.js
@@ -0,0 +1,11 @@
+/*
+write a function that checks and returns the input's type
+
+ex: if the input is a function, it should return 'function'
+if the input is an array, it should return 'array'
+if the argument is missing, it should return => 'no input provided'
+*/
+
+const typeCheck = input => {
+  // your code here
+}
