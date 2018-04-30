3cfe0356e6a99e87a6f0e4eee344ad1d5b7df2f1
Kevin Tamarus Tang <kevintamarus@gmail.com>
2018-04-29 23:10:34 -0700

    palindrome sentence challenge added

---
+++ functional-programming-3.js
@@ -0,0 +1,18 @@
+/*
+given a sentence, find the longest palindrome.
+Use a series of functions to complete this challenge!
+
+"this is a racecar with no radar, its nickname is tacocat" => "tacocat"
+*/
+
+const longestPalindrome = sentence => {
+  // your code here
+}
+
+const reverseString = str => {
+  // your code here
+}
+
+const isPalindrome = str => {
+  // your code here
+}
