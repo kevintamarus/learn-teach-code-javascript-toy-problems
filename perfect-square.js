075b68ad950ed6db71a30cb80ba8eefd35f26273
Kevin Tamarus Tang <kevintamarus@gmail.com>
2018-05-01 23:29:48 -0700

    perfect square problem added

---
+++ perfect-square.js
@@ -0,0 +1,14 @@
+/*
+Write a function that will return true if the input is a perfect square, and will return false if it's not
+
+4 => true
+16 => true
+0 => true
+5 => false
+27 => false
+-16 => false
+*/
+
+const isPerfectSquare = n => {
+  // your code here
+}
