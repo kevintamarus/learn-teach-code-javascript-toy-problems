def2413aa6394f4f58af42d08a318900b4da3acd
Kevin Tamarus Tang <kevintamarus@gmail.com>
2018-05-02 23:11:27 -0700

    random dice challenge added

---
+++ random-dice.js
@@ -0,0 +1,11 @@
+/*
+Can you write a function that will generate a random throw of two dice?
+The odds should be consistent with throwing two dice:
+Ex: there should be a 6/36 chance that a 7 is thrown, there should be a 1/36 chance that a 2 or 12 is thrown
+
+output possibilities => 2, 3, 4, 5, 6, 7, 8, 9, 10. 11, 12
+*/
+
+const throwDice = () => {
+  // your code here
+}
