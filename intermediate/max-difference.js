a39d934d96364f0366593087c13e22c7ddbc75fb
Kevin Tamarus Tang <kevintamarus@gmail.com>
2018-05-11 21:01:51 -0700

    max difference question added

---
+++ intermediate/max-difference.js
@@ -0,0 +1,16 @@
+/*
+Given an array of numbers, write a function that will find the max positive difference in that array
+
+[1, 2, 3, 4, 5] => 1
+[5, 3, 2, 1, 8] => 7
+[4, 9, 2, 3, 6] => 5
+*/
+
+const maxDifference = arr => {
+  // your code here
+}
+
+// can you solve it using O(n) linear time complexity?
+const maxDifference = arr => {
+  // your code here
+}
