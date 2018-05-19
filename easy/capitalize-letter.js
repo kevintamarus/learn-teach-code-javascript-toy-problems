f6e3a392953c2a33cf331aedbc9e39b8e5a5682b
Kevin Tamarus Tang <kevintamarus@gmail.com>
2018-05-19 14:42:13 -0700

    capitalize letter problem added

---
+++ easy/capitalize-letter.js
@@ -0,0 +1,12 @@
+/*
+Given a string, write a function that will return the string with the first letter capitalized
+
+'john' => 'John'
+'master' => 'Master'
+'Boss' => 'Boxx'
+'' => ''
+*/
+
+const capitalizeWord = str => {
+  // your code here
+}
