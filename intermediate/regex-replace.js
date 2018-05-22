62ee0e63c500fccccc3ea9094df9ab4cbae18e22
Kevin Tamarus Tang <kevintamarus@gmail.com>
2018-05-21 22:59:15 -0700

    regex replace challenge added

---
+++ intermediate/regex-replace.js
@@ -0,0 +1,13 @@
+/*
+Given a sentence(string), replace any 'foo' words in the sentence with 'bar'
+
+hint: you can use RegEx
+
+"These words cannot foo me" => "These words cannot bar me"
+*/
+
+const exampleSentence = "These words cannot foo me"
+
+const capitalizeWord = sentence => {
+  // your code here
+}
