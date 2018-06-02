313b386a606f655440458b23391f3ecfb7a87112
Kevin Tamarus Tang <kevintamarus@gmail.com>
2018-06-01 17:36:30 -0700

    array flip added

---
+++ intermediate/array-flip.js
@@ -0,0 +1,24 @@
+/*
+Given an array of array, write a function that will flip the array upside down.
+
+input:
+[
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9]
+]
+
+output:
+[
+  [9, 8, 7],
+  [6, 5, 4],
+  [3, 2, 1]
+]
+*/
+
+//sample arry
+const array = [[1, 2, 3], [4, 5, 6], [7, 8, 9]];
+
+const flipArray = arr => {
+  // your code here
+}
